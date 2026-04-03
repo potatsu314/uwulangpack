9e9b0a2bbddcd5a84746180fd48cbc386ed661a9
potatsu314 <Mpotatsu+github@protonmail.com>
2026-04-03 14:15:07 +0300

    expanded vocabulary, added seed for random to make conversion more predictable

--- libuwuify.py
+++ libuwuify.py
@@ -2,32 +2,41 @@ import random
 KAOMOJIS = [":3", "UwU", "OwO", ">w<", "~w~", "qwq", ">~<", ":3c", ">:3", ">:3c"]
 VOCAB = {
     "love": "wuv",
-    "friend": "fwiend",
+    "Love": "Wuv",
+    "friend": "fwend",
+    "Friend": "Fwend",
     "good": "gud",
-    "what": "wut"
+    "Good": "Gud",
+    "what": "wut",
+    "What": "Wut",
+    "hand": "paw",
+    "hands": "paws",
+    "Hand": "Paw",
+    "Hands": "Paws",
+    "feet": "paws",
+    "Feet": "Paws"
 }
 
-def uwuify(inp: str, *args, use_letter_change: bool = True, use_kaomojis: bool = True, use_squiggly_lines: bool = False, use_stutter: bool = True, use_vocab: bool = True):
-    if use_vocab:
-        inp_tmp = inp.split(" ")
+def uwuify(inp: str, *args, random_seed: str | int | float | None = None, use_letter_change: bool = True, use_kaomojis: bool = True, use_squiggly_lines: bool = False, use_stutter: bool = True, use_vocab: bool = True):
+    if len(inp) == 0: return ""
 
-        for i in range(0, len(inp_tmp)):
-            if inp_tmp[i].lower() in VOCAB:
-                rep = VOCAB[inp_tmp[i].lower()]
-                if inp_tmp[i][0].isupper():
-                    rep = rep[0].upper() + rep[1:]
-                inp_tmp[i] = rep
-        
-        inp = " ".join(inp_tmp)
-        
-        inp = " ".join(inp_tmp)
+    r = None
+
+    if random_seed is not None:
+        r = random.Random(random_seed)
+    else:
+        r = random
+    
+    if use_vocab:
+        for i in VOCAB.keys():
+            inp = inp.replace(i, VOCAB[i])
     if use_letter_change:
         inp = inp.replace("r", "w").replace("l", "w").replace("R", "W").replace("L", "W")
     if use_stutter:
         inp_tmp = inp.split(" ")
 
         for i in range(0, len(inp_tmp)):
-            if inp_tmp[i].isalpha() and random.randint(0, 6) == 0:
+            if inp_tmp[i].isalpha() and r.randint(0, 6) == 0:
                 inp_tmp[i] = inp_tmp[i][0] + "-" + inp_tmp[i]
         
         inp = " ".join(inp_tmp)
@@ -35,12 +44,12 @@ def uwuify(inp: str, *args, use_letter_change: bool = True, use_kaomojis: bool =
         inp_tmp = inp.split(" ")
 
         for i in range(0, len(inp_tmp)):
-            if random.randint(0, 5) == 0:
+            if r.randint(0, 5) == 0:
                 inp_tmp[i] += "~"
 
         inp = " ".join(inp_tmp)
     if use_kaomojis:
-        inp = inp + " " + random.choice(KAOMOJIS)
+        inp = inp + " " + r.choice(KAOMOJIS)
     
     return inp
 

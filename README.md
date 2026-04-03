7d547c7bb26a8f65fef4bcfcc7254969f6afff53
potatsu314 <Mpotatsu+github@protonmail.com>
2026-04-03 14:18:26 +0300

    made readme "normal", added support for end credits and splashes (for minecraft namespace only), restructured code a little bit, search for english localization now uses regexp

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # uwulangpack
-Toow that twansfowms twanswation text fwom client.jar and mod.jar and resourcepack.zip to uwuified vewsion! >w<
-The code is pwetty twash, but i-it wowks ig. UwU
+Tool that transforms translation texts from client.jar and mod.jar and resourcepack.zip to uwuified version (only supports english)!
+
+The code is pretty trash, but it works ig.
 
 ## Usage
-B-Basic usage :3
+Basic usage :3
 ```
-./uwuify.py -lkscv --pack-mcmeta pack.mcmeta --pack-png pack.png --input client.jar mod1.jar mod2.jar --output uwulang.zip
+./uwuify.py -lksc --texts --pack-mcmeta pack.mcmeta --pack-png pack.png --input client.jar mod1.jar mod2.jar --output uwulang.zip
 ```
 
-Ow just get some hewp :D
+Or just get some help :D
 ```
 ./uwuify.py --help
 ```
--- uwuify.py
+++ uwuify.py
@@ -1,10 +1,11 @@
-#!/bin/python
+#!python
 
 import argparse
 import json
 import os
 import sys
 import zipfile
+import re
 
 from libuwuify import uwuify
 
@@ -16,6 +17,7 @@ parser.add_argument("-k", "--kaomojis", action="store_true", help="add kaomojis 
 parser.add_argument("-s", "--stutter", action="store_true", help="add random stutters to words")
 parser.add_argument("-q", "--squiggly", action="store_true", help="add random ~ to words")
 parser.add_argument("-c", "--vocab", action="store_true", help="replace some words with predefined uwuified words")
+parser.add_argument("--texts", action="store_true", help="translate vanilla splash/end credits text too")
 parser.add_argument("--pack-mcmeta", type=str, help="pack.mcmeta for a resource pack")
 parser.add_argument("--pack-png", type=str, help="pack.png for a resource pack (pls make sure that it is a png)")
 parser.add_argument("--input", type=str, required=True, nargs="+", help="path to your en_us.json (only for vanilla) or .jar files (mods or minecraft version) or .zip files (resourcepacks)")
@@ -23,64 +25,69 @@ parser.add_argument("--output", type=str, help="output path for resource pack", 
 
 args = parser.parse_args()
 
-def parse_zip(ifp: str):
+def parse_zip(ifp: str) -> tuple[list[tuple[str, dict]], tuple[str | None, str | None]]:
     f = zipfile.ZipFile(ifp, "r")
     files = f.namelist()
     
-    fp = None
-    for j in files:
-        if j.endswith("lang/en_us.json"): # TODO: make it better or smth
-            fp = j
-            break
-    
-    if fp is None:
-        if args.verbose: print(f"could not find en_us.json in {i}") 
-        return None
+    ret = []
+    alr = []
 
-    inp_lang = json.loads(f.read(fp).decode("utf-8"))
-    inp_name = fp.removeprefix("assets/").removesuffix("/lang/en_us.json").strip()
+    for j in files:
+        m = re.search(r"^assets/([^/]+)/lang/en_\w+.json$", j)
+        if m and m.group(1) not in alr:
+            alr.append(m.group(1))
+            ret.append((m.group(1), json.loads(f.read(j).decode("utf-8"))))
+    
+    end = None
+    splash = None
+    if "assets/minecraft/texts/end.txt" in files:
+        end = f.read("assets/minecraft/texts/end.txt").decode("utf-8")
+    if "assets/minecraft/texts/splashes.txt" in files:
+        splash = f.read("assets/minecraft/texts/splashes.txt").decode("utf-8")        
+
     f.close()
-    return (inp_lang, inp_name)
+    return (ret, (end, splash))
 
 locales = {}
+end = None
+splash = None
 
 for i in args.input:
+    if args.verbose: print(f"parsing {i}")
     if not os.path.exists(i):
-        if args.verbose: print(f"invalid file path: {i}")
+        print(f"invalid file path: {i}")
         continue
 
-    inp_lang = None
-    inp_name = None
+    to_add = []
 
     if i.endswith(".json"):
-        inp_lang = json.load(open(args.input, "r"))
-        inp_name = "minecraft"
+        to_add.append(("minecraft", json.load(open(args.input, "r"))))
 
     elif i.endswith(".jar") or i.endswith(".zip"):
         z = parse_zip(i)
+        to_add.extend(z[0])
 
-        if z is None:
-            continue
+        if z[1][0] is not None:
+            end = z[1][0]
+        if z[1][1] is not None:
+            splash = z[1][1]
 
-        inp_lang = z[0]
-        inp_name = z[1]
     else:
-        if args.verbose: print(f"unsupported file format: {i}")
+        print(f"unsupported file format: {i}")
         continue
 
-    if inp_lang is None or inp_name is None:
-        if args.verbose: print(f"couldn't parse file: {i}")
-        continue
+    for j in to_add:
+        if j[0] not in locales:
+            locales[j[0]] = {}
+        
+        locales[j[0]] |= j[1]
 
-    out = {}
-
-    for i in inp_lang.keys():
-        out[i] = uwuify(inp_lang[i], use_letter_change=args.letters, use_kaomojis=args.kaomojis, use_squiggly_lines=args.squiggly, use_stutter=args.stutter, use_vocab=args.vocab)
-
-    if len(out) > 0:
-        locales[inp_name] = out
+for i in locales.keys():
+    for j in locales[i].keys():
+        locales[i][j] = uwuify(locales[i][j], random_seed=j, use_letter_change=args.letters, use_kaomojis=args.kaomojis, use_squiggly_lines=args.squiggly, use_stutter=args.stutter, use_vocab=args.vocab)
 
 if len(locales) == 0:
+    print("couldn't find any translation files")
     sys.exit()
 
 f = zipfile.ZipFile(args.output, "w")
@@ -109,4 +116,14 @@ for i in locales.keys():
     f.mkdir(f"assets/{i}")
     f.mkdir(f"assets/{i}/lang")
     f.writestr(f"assets/{i}/lang/en_uwu.json", json.dumps(locales[i]))
+if args.texts and (end is not None or splash is not None):
+    def convert_multiline_string(inp: str) -> str:
+        return "\n".join([uwuify(x, random_seed=x, use_letter_change=args.letters, use_kaomojis=args.kaomojis, use_squiggly_lines=args.squiggly, use_stutter=args.stutter, use_vocab=args.vocab) for x in inp.splitlines()])
+    
+    f.mkdir("assets/minecraft/texts")
+    if end is not None:
+        f.writestr("assets/minecraft/texts/end.txt", convert_multiline_string(end))
+    if splash is not None:
+        f.writestr("assets/minecraft/texts/splashes.txt", convert_multiline_string(splash))
+    
 f.close()

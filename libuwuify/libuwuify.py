20b7126cdb6e32fb1d7ae87d0cb28dff738656c1
potatsu314 <Mpotatsu+github@protonmail.com>
2026-04-04 18:28:06 +0300

    Added support for multiple languages (not only english) and some code restructuring

---
+++ libuwuify/libuwuify.py
@@ -0,0 +1,69 @@
+import random
+import typing
+import os
+import json
+
+class LangRules(typing.TypedDict):
+    vocab: dict[str, str]
+    letter_replacements: dict[str, str]
+    kaomojis: list[str]
+
+class Lang:
+    def __init__(self) -> None:
+        self.s: dict[str, LangRules] = {}
+
+        for i in os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lang-rules")):
+            if os.path.isfile(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lang-rules", i)):
+                self.s[i.removesuffix(".json")] = json.load(open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lang-rules", i), "r"))
+    
+    def __getitem__(self, key: str) -> LangRules:
+        if key not in self.s:
+            print(self.s)
+        return self.s[key]
+
+LANG = Lang()
+
+def uwuify(inp: str, *args, random_seed: str | int | float | None = None, lang_rules: LangRules = LANG["en_us"], use_letter_change: bool = True, use_kaomojis: bool = True, use_squiggly_lines: bool = False, use_stutter: bool = True, use_vocab: bool = True, use_lower_case: bool = False):
+    if len(inp) == 0: return ""
+
+    r = None
+
+    if random_seed is not None:
+        r = random.Random(random_seed)
+    else:
+        r = random
+    
+    if use_vocab:
+        for i in lang_rules["vocab"].keys():
+            inp = inp.replace(i, lang_rules["vocab"][i])
+    if use_lower_case:
+        inp = inp.lower()
+    if use_letter_change:
+        for i in lang_rules["letter_replacements"].keys():
+            inp = inp.replace(i, lang_rules["letter_replacements"][i])
+    if use_stutter:
+        inp_tmp = inp.split(" ")
+
+        for i in range(0, len(inp_tmp)):
+            if inp_tmp[i].isalpha() and r.randint(0, 6) == 0:
+                inp_tmp[i] = inp_tmp[i][0] + "-" + inp_tmp[i]
+        
+        inp = " ".join(inp_tmp)
+    if use_squiggly_lines:
+        inp_tmp = inp.split(" ")
+
+        for i in range(0, len(inp_tmp)):
+            if r.randint(0, 5) == 0:
+                inp_tmp[i] += "~"
+
+        inp = " ".join(inp_tmp)
+    if use_kaomojis:
+        inp = inp + " " + r.choice(lang_rules["kaomojis"])
+    
+    return inp
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        for i in sys.argv[1].splitlines():
+            print(uwuify(i))
--- uwuify.py
+++ uwuify.py
@@ -7,9 +7,9 @@ import sys
 import zipfile
 import re
 
-from libuwuify import uwuify
+import libuwuify.libuwuify as libuwuify
 
-parser = argparse.ArgumentParser(prog="uwuify", description="converts minecraft english translation to english but uwu")
+parser = argparse.ArgumentParser(prog="uwuify", description="converts minecraft localization to uwuified version")
 
 parser.add_argument("-v", "--verbose", action="store_true", help="show some debug information")
 parser.add_argument("-l", "--letters", action="store_true", help="change letters l, r to w")
@@ -21,23 +21,29 @@ parser.add_argument("-w", "--lowercase", action="store_true", help="change all l
 parser.add_argument("--texts", action="store_true", help="translate vanilla splash/end credits text too")
 parser.add_argument("--pack-mcmeta", type=str, help="pack.mcmeta for a resource pack")
 parser.add_argument("--pack-png", type=str, help="pack.png for a resource pack (pls make sure that it is a png)")
-parser.add_argument("--input", type=str, required=True, nargs="+", help="path to your en_us.json (only for vanilla) or .jar files (mods or minecraft version) or .zip files (resourcepacks)")
+parser.add_argument("--language", type=str, help="language to translate from", default="en_us")
+parser.add_argument("--asset-index", type=str, help="name of index.json in assets/indexes/ (29.json for example)")
+parser.add_argument("--input", type=str, required=True, nargs="+", help="path to your en_us.json (only for vanilla) or .jar files (mods or minecraft version) or .zip files (resourcepacks) or assets folder in .minecraft")
 parser.add_argument("--output", type=str, help="output path for resource pack", default="uwulang.zip")
 
 args = parser.parse_args()
 
-def parse_zip(ifp: str) -> tuple[list[tuple[str, dict]], tuple[str | None, str | None]]:
-    f = zipfile.ZipFile(ifp, "r")
+def parse_zip(path_archive: str) -> tuple[list[tuple[str, dict]], tuple[str | None, str | None]] | None:
+    f = zipfile.ZipFile(path_archive, "r")
     files = f.namelist()
     
     ret = []
-    alr = []
 
     for j in files:
-        m = re.search(r"^assets/([^/]+)/lang/en_\w+.json$", j)
-        if m and m.group(1) not in alr:
-            alr.append(m.group(1))
-            ret.append((m.group(1), json.loads(f.read(j).decode("utf-8"))))
+        m = re.search(r"^assets/([^/]+)/lang/{}.json$".format(args.language), j)
+        if m:
+            try:
+                j = json.loads(f.read(j).decode("utf-8"))
+                ret.append((m.group(1), j))
+            except Exception as e:
+                if args.verbose: 
+                    print(e)
+                return None
     
     end = None
     splash = None
@@ -48,6 +54,17 @@ def parse_zip(ifp: str) -> tuple[list[tuple[str, dict]], tuple[str | None, str |
 
     f.close()
     return (ret, (end, splash))
+
+def parse_assets(path: str, index_name: str) -> dict | None:
+    index_file = json.load(open(os.path.join(path, "indexes", index_name), "r"))
+    if f"minecraft/lang/{args.language}.json" not in index_file["objects"]:
+        return None
+    
+    h = index_file["objects"][f"minecraft/lang/{args.language}.json"]["hash"]
+    
+    fp = os.path.join(path, "objects", h[0:2], h)
+
+    return json.load(open(fp, "r"))
 
 locales = {}
 end = None
@@ -66,12 +83,21 @@ for i in args.input:
 
     elif i.endswith(".jar") or i.endswith(".zip"):
         z = parse_zip(i)
+
+        if z is None:
+            continue
+
         to_add.extend(z[0])
 
         if z[1][0] is not None:
             end = z[1][0]
         if z[1][1] is not None:
             splash = z[1][1]
+    
+    elif os.path.isdir(i) and os.path.exists(os.path.join(i, "indexes")) and os.path.exists(os.path.join(i, "objects")):
+        a = parse_assets(i, args.asset_index)
+        if a is not None:
+            to_add.append(("minecraft", a))
 
     else:
         print(f"unsupported file format: {i}")
@@ -85,7 +111,7 @@ for i in args.input:
 
 for i in locales.keys():
     for j in locales[i].keys():
-        locales[i][j] = uwuify(locales[i][j], random_seed=j, use_letter_change=args.letters, use_kaomojis=args.kaomojis, use_squiggly_lines=args.squiggly, use_stutter=args.stutter, use_vocab=args.vocab, use_lower_case=args.lowercase)
+        locales[i][j] = libuwuify.uwuify(locales[i][j], random_seed=j, lang_rules=libuwuify.LANG[args.language], use_letter_change=args.letters, use_kaomojis=args.kaomojis, use_squiggly_lines=args.squiggly, use_stutter=args.stutter, use_vocab=args.vocab, use_lower_case=args.lowercase)
 
 if len(locales) == 0:
     print("couldn't find any translation files")
@@ -101,7 +127,7 @@ else:
             "pack_format": 15,
         },
         "language": {
-            "en_uwu": {
+            "en_us_uwu": {
                 "name": "Engwish~ :3",
                 "region": "Furry",
                 "bidirectional": False
@@ -116,19 +142,19 @@ f.mkdir("assets")
 for i in locales.keys():
     f.mkdir(f"assets/{i}")
     f.mkdir(f"assets/{i}/lang")
-    f.writestr(f"assets/{i}/lang/en_uwu.json", json.dumps(locales[i]))
+    f.writestr(f"assets/{i}/lang/{args.language}_uwu.json", json.dumps(locales[i]))
 if args.texts and (end is not None or splash is not None):
     def convert_multiline_string(inp: str) -> str:
         ret = []
         for i in inp.splitlines():
             if "PLAYERNAME" not in i:
-                ret.append(uwuify(i, random_seed=i, use_letter_change=args.letters, use_kaomojis=args.kaomojis, use_squiggly_lines=args.squiggly, use_stutter=args.stutter, use_vocab=args.vocab, use_lower_case=args.lowercase))
+                ret.append(libuwuify.uwuify(i, random_seed=i, use_letter_change=args.letters, use_kaomojis=args.kaomojis, use_squiggly_lines=args.squiggly, use_stutter=args.stutter, use_vocab=args.vocab, use_lower_case=args.lowercase))
             else:
                 s = i.split("PLAYERNAME")
                 for i in range(len(s) - 1):
-                    s[i] = uwuify(s[i], random_seed=s[i], use_letter_change=args.letters, use_kaomojis=False, use_squiggly_lines=args.squiggly, use_stutter=args.stutter, use_vocab=args.vocab, use_lower_case=args.lowercase)
+                    s[i] = libuwuify.uwuify(s[i], random_seed=s[i], use_letter_change=args.letters, use_kaomojis=False, use_squiggly_lines=args.squiggly, use_stutter=args.stutter, use_vocab=args.vocab, use_lower_case=args.lowercase)
                     
-                s[-1] = uwuify(s[-1], random_seed=s[-1], use_letter_change=args.letters, use_kaomojis=args.kaomojis, use_squiggly_lines=args.squiggly, use_stutter=args.stutter, use_vocab=args.vocab, use_lower_case=args.lowercase)
+                s[-1] = libuwuify.uwuify(s[-1], random_seed=s[-1], use_letter_change=args.letters, use_kaomojis=args.kaomojis, use_squiggly_lines=args.squiggly, use_stutter=args.stutter, use_vocab=args.vocab, use_lower_case=args.lowercase)
                 ret.append("PLAYERNAME".join(s))
         return "\n".join(ret)
     
